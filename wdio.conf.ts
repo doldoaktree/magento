fbdeb4378875593a3a0332b2ec2583d1af78603b
doldoaktree <doldoaktree@gmail.com>
2024-05-23 07:46:45 +1000

    changed reporter to spec

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -135,7 +135,7 @@ export const config: Options.Testrunner = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['junit'],
+    reporters: ['spec'],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
